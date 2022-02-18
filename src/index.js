43645d957c30776907455bf4efb1d1f81cceb8fe
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2022-02-18 17:34:25 -0300

    desenvolve o projeto ao vivo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
-console.log(validator);
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const div = document.querySelector('[data-valid]');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const value = input.value;
+  const isValid = validator.isValid(value);
+  div.dataset.valid = isValid;
+});
